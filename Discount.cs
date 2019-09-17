67feb81d7cfda3dee5620938ed5da8ca69af9382
Tattersson <tatupaski@outlook.com>
2019-09-17 11:40:06 +0300

    Program 1.3
    Signed-off-by: Tattersson <tatupaski@outlook.com>

---
+++ Discount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hello
+{
+    static int Main()
+    {
+                //Tässä kysyn asikkaalla summaa
+                Console.WriteLine("Anna laskun summa:");
+                int lasku = Convert.ToInt32(Console.ReadLine());
+
+                //Tässä kysyn alennuksen prosentti määrää
+                Console.WriteLine("Anna alennuksen prosentti määrä:");
+                int alennus = Convert.ToInt32(Console.ReadLine());
+
+                //Tässä kerron koko summan alennuksineen
+                int alennusmsg = lasku - (lasku * alennus / 100);
+                Console.WriteLine(value: "After discount your bill is: " + alennusmsg);
+
+    }
+}
