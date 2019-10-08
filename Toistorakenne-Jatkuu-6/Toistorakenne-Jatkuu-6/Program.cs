7d532ccee79e1371b7d9ceb18b3164b583ef1b9e
Tattersson <tatupaski@outlook.com>
2019-10-08 20:57:41 +0300

    Commit

---
+++ Toistorakenne-Jatkuu-6/Toistorakenne-Jatkuu-6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Toistorakenne_Jatkuu_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("Toistorakenne-Jatkuu-6 | Tehtävä");
+            int kuusi = 0;
+
+            for (int n = 0; n <= 1000; n++)
+            {
+                int tulos = rnd.Next(6);
+
+
+                Console.WriteLine($"{n}. {tulos+1}");
+                if (tulos + 1 == 6)
+                {
+                    kuusi++;
+                }
+              
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Numero kuusi arvottiin {kuusi} kertaa.");
+            return;
+           
+        }
+    }
+}
