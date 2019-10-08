6e7f425f0ac49a747eddfaa3cc727bbb667cdc4b
Tattersson <tatupaski@outlook.com>
2019-10-08 20:09:47 +0300

    Versio 1.2

--- Toistorakenne-Jatkuu-3/Toistorakenne-Jatkuu-3/Program.cs
+++ Toistorakenne-Jatkuu-3/Toistorakenne-Jatkuu-3/Program.cs
@@ -6,7 +6,27 @@ namespace Toistorakenne_Jatkuu_3
     {
         static void Main(string[] args)
         {
-            
+            Random rnd = new Random();
+            int i = 0;
+            int n = 1;
+
+            while (n < 5)
+            {
+                Console.Write("Rivi " + n + ": ");
+                while (i < 4)
+                {
+                    Console.Write(rnd.Next(50) + ", ");
+                    i++;
+                    if (i == 4)
+                    {
+                        Console.Write(rnd.Next(50) + ".");
+                    }
+                }
+                i = 0;
+                n++;
+                Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
