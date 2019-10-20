897fde99da84d27286d40b790873abfa768ca61e
Tattersson <tatupaski@outlook.com>
2019-10-20 23:51:43 +0300

    Re-Filing programs
    Signed-off-by: Tattersson <tatupaski@outlook.com>

---
+++ IF-harjoitukset/IF1.2/EvenorOdd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace check1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i;
+            Console.WriteLine("Enter a Number : ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Entered Number is an Even Number");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Entered Number is an Odd Number");
+                Console.Read();
+            }
+        }
+    }
+}
+
