fca95052e19e03be04b42938e60fe9ff76f38c16
Tattersson <tatupaski@outlook.com>
2019-11-03 22:49:05 +0200

    New program

---
+++ Funktio-ja-aliohjelmat-harjoitukset/funktiot-ja-aliohjelmat-2/funktiot-ja-aliohjelmat-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace funktiot_ja_aliohjelmat_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna kaksi lukua. Ohjelma tulostaa pienemmän!");
+            Console.Write("Luku 1: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Luku 2: ");
+            int luku2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(pieni(luku1, luku2));
+        }
+        static string pieni(int luku1, int luku2)
+        {
+            string lause = "";
+            if (luku1 < luku2)
+            {
+                Console.WriteLine($"Luku {luku1} on pienempi kuin {luku2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Luku {luku2} on pienempi kuin {luku1}.");
+            }
+            return lause;
+        }
+    }
+
+}
